762797535f6501bdeb336a332dfb3aa9ea306350
Shashwath <shashwathbc@gmail.com>
2021-12-21 23:05:24 +0530

    commit

---
+++ javascript/operators.js
@@ -0,0 +1,63 @@
+// Operator types in js
+
+
+// 1. Arithmetic operators: + - / % * ++ --
+var x=10;
+var y =20;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x%y);   
+
+console.log(x/y);
+
+console.log(x*y);
+
+//increment: two types:
+// 1)post: x++ firstly returns the current value then  elemnt 
+// 2) pre: ++x: firstly increment then give u the current value
+console.log(x++);
+console.log(++x);
+
+//decrement: two types:
+// 1)post: x-- firstly returns the current value then  decrement
+// 2) pre: --x: firstly decrement then give u the current value
+
+console.log(x--);
+console.log(--x);
+
+
+
+// 2. Comparison operators:  == ===  != !== > >= < <=
+
+
+console.log(x!=y);
+console.log(x!==y);   // checks for type also
+console.log(x==y);
+console.log(x===y);
+console.log(x>y);
+console.log(x<y);
+
+
+
+// 3. Bitwise operators:   &(and)  , |(or)  , (xor) , (nor)
+// 1-0001
+// 2-0010
+// 3-0011
+// 4-0100
+
+
+// 0 & 0 = 0
+// 0 & 1 = 0
+// 1 & 0 = 0
+// 1 & 1 = 1
+
+
+
+
+
+
+
+
