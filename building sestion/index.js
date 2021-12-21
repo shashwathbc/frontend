42a9089ee2e02b5a65687f1072dd2564af2513bd
Shashwath <shashwathbc@gmail.com>
2021-12-21 20:37:21 +0530

    commit

---
+++ building sestion/index.js
@@ -0,0 +1,27 @@
+var num1Input = document.getElementById("num1")
+var num2Input = document.getElementById("num2")
+var operator = document.getElementById("operator")
+var result = document.getElementById("result")
+
+function calculate(){
+    if(num1Input.value===""){
+        alert("please enter number 1")
+    }
+    if(num2Input.value===""){
+        alert("Please enter number 2")
+    }
+var num1 = Number(num1Input.value)
+var num2 = Number(num2Input.value)
+
+var op=operator.value
+if(op==="-"){
+    result.value= num1-num2
+}else if(op==="*"){
+    result.value= num1*num2
+}else if(op==="/"){
+    result.value= num1/num2
+}else{
+    result.value= num1+num2
+}
+
+}
