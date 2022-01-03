c3e165b5a2eac1f80c3b3cf04aa12c004494e9c9
Shashwath <shashwathbc@gmail.com>
2022-01-03 19:41:32 +0530

    commit

--- DSA JAVASCRIPT/Basics.js
+++ DSA JAVASCRIPT/Basics.js
@@ -1,108 +1,146 @@
-//Add,sub,mul,div: Two Numbers with JavaScript
-const sum = 10 + 10;
-const difference = 45 - 33;
-const product = 8 * 10;
-const quotient = 66 / 33;
+// // //Add,sub,mul,div: Two Numbers with JavaScript
+// // const sum = 10 + 10;
+// // const difference = 45 - 33;
+// // const product = 8 * 10;
+// // const quotient = 66 / 33;
 
-console.log(sum);
-console.log(difference);
-console.log(product);
-console.log(quotient);
-
-
-//increnment: 
-// You can easily increment or add one to a variable with the ++ operator.
-// i++;
-// is the equivalent of
-// i = i + 1;
-// **********************
-let myVar = 87;
-myVar++;
-console.log(myVar);
+// // console.log(sum);
+// // console.log(difference);
+// // console.log(product);
+// // console.log(quotient);
 
 
-// Decrement a Number with JavaScript:
-// You can easily decrement or decrease a variable by one with the -- operator.
-// i--;
-// is the equivalent of
-// i = i - 1;
-//************************* */
-let myar = 11;
-myar--;
-console.log(myar);
+// // //increnment: 
+// // // You can easily increment or add one to a variable with the ++ operator.
+// // // i++;
+// // // is the equivalent of
+// // // i = i + 1;
+// // // **********************
+// // let myVar = 87;
+// // myVar++;
+// // console.log(myVar);
 
 
-
-
-// Create Decimal Numbers with JavaScript:
-// We can store decimal numbers in variables too.
-// Decimal numbers are sometimes referred to as floating point numbers or floats.
-let myDecimal=5.7;
-console.log(myDecimal);
-//Multiply Two Decimals with JavaScript
-const mul = 2.0 * 2.5;
-console.log(mul);
-//Divide One Decimal by Another with JavaScript
-const quotnt = 4.4 / 2.0;
-console.log(quotnt);
-//Finding a Remainder in JavaScript:
-const remaindr = 11%3;
-console.log(remaindr);
-
-//Compound Assignment With Augmented Addition:
-// In programming, it is common to use assignments to modify the contents of a variable. 
-// Remember that everything to the right of the equals sign is evaluated first, so we can say:
-// myVar = myVar + 5;
-// to add 5 to myVar. Since this is such a common pattern, 
-// there are operators which do both a mathematical operation and assignment in one step.
-// One such operator is the += operator.
-// let myVar = 1;
-// myVar += 5;
-// console.log(myVar);
-// 6 would be displayed in the console.
-let a = 3;
-let b = 17;
-let c = 12;
-a += 12;
-b += 9;
-c +=  7;
-//Compound Assignment With Augmented Subtraction
-let a = 11;
-let b = 9;
-let c = 3;
-a -= 6;
-b -= 15;
-c -= 1;
-//Compound Assignment With Augmented Multiplication
-let a = 5;
-let b = 12;
-let c = 4.6;
-a *= 5;
-b *= 3 ;
-c *= 10;
-//Compound Assignment With Augmented Division
-let a = 48;
-let b = 108;
-let c = 33;
-a /=12;
-b /= 4;
-c /= 11;
-
-//Escaping Literal Quotes in Strings:
+// // // Decrement a Number with JavaScript:
+// // // You can easily decrement or decrease a variable by one with the -- operator.
+// // // i--;
+// // // is the equivalent of
+// // // i = i - 1;
+// // //************************* */
+// // let myar = 11;
+// // myar--;
+// // console.log(myar);
 
 
 
 
+// // // Create Decimal Numbers with JavaScript:
+// // // We can store decimal numbers in variables too.
+// // // Decimal numbers are sometimes referred to as floating point numbers or floats.
+// // let myDecimal=5.7;
+// // console.log(myDecimal);
+// // //Multiply Two Decimals with JavaScript
+// // const mul = 2.0 * 2.5;
+// // console.log(mul);
+// // //Divide One Decimal by Another with JavaScript
+// // const quotnt = 4.4 / 2.0;
+// // console.log(quotnt);
+// // //Finding a Remainder in JavaScript:
+// // const remaindr = 11%3;
+// // console.log(remaindr);
+
+// // //Compound Assignment With Augmented Addition:
+// // // In programming, it is common to use assignments to modify the contents of a variable. 
+// // // Remember that everything to the right of the equals sign is evaluated first, so we can say:
+// // // myVar = myVar + 5;
+// // // to add 5 to myVar. Since this is such a common pattern, 
+// // // there are operators which do both a mathematical operation and assignment in one step.
+// // // One such operator is the += operator.
+// // // let myVar = 1;
+// // // myVar += 5;
+// // // console.log(myVar);
+// // // 6 would be displayed in the console.
+// // let a = 3;
+// // let b = 17;
+// // let c = 12;
+// // a += 12;
+// // b += 9;
+// // c +=  7;
+// // //Compound Assignment With Augmented Subtraction
+// // let a = 11;
+// // let b = 9;
+// // let c = 3;
+// // a -= 6;
+// // b -= 15;
+// // c -= 1;
+// // //Compound Assignment With Augmented Multiplication
+// // let a = 5;
+// // let b = 12;
+// // let c = 4.6;
+// // a *= 5;
+// // b *= 3 ;
+// // c *= 10;
+// // //Compound Assignment With Augmented Division
+// // let a = 48;
+// // let b = 108;
+// // let c = 33;
+// // a /=12;
+// // b /= 4;
+// // c /= 11;
+
+
+// //Escaping Literal Quotes in Strings:
+// const lit = "I am a \"double quoted\" string inside \"double quotes\".";
+// console.log(lit);
+
+
+
+// //Quoting Strings with Single Quotes:
+// var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+
+// // Escape Sequences in Strings:
+// // Quotes are not the only characters that can be escaped inside a string. 
+// // There are two reasons to use escaping characters:
+
+// // To allow you to use characters you may not otherwise be able to type out, such as a carriage return.
+// // To allow you to represent multiple quotes in a string without JavaScript misinterpreting what you mean.
+// // We learned this in the previous challenge.
+
+// // Code	Output
+// // \'	single quote
+// // \"	double quote
+// // \\	backslash
+// // \n	newline
+// // \r	carriage return
+// // \t	tab
+// // \b	word boundary
+// // \f	form feed
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+// // o/p:
+// // FirstLine
+// //     \SecondLine
+// // ThirdLine
 
 
 
 
+//Concatenating Strings with Plus Operator
+
+const conCat = "This is the start. "+"This is the end.";
+console.log(conCat);
 
 
+//Concatenating Strings with the Plus Equals Operator:
+let myStr="This is the first sentence. ";
+myStr += "This is the second sentence.";
 
+//Constructing Strings with Variables:
+const myName = "Shashwath";
+const myStr = "My name is" + myName + ",and I am well!";
 
-
-
+//Appending Variables to Strings:
 
 
 
