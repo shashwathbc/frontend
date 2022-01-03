7824beef710bd23ad52f70b09ed98d3935acc83a
Shashwath <shashwathbc@gmail.com>
2022-01-03 17:44:04 +0530

    commit

---
+++ DSA JAVASCRIPT/Basics.js
@@ -0,0 +1,108 @@
+//Add,sub,mul,div: Two Numbers with JavaScript
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+
+
+//increnment: 
+// You can easily increment or add one to a variable with the ++ operator.
+// i++;
+// is the equivalent of
+// i = i + 1;
+// **********************
+let myVar = 87;
+myVar++;
+console.log(myVar);
+
+
+// Decrement a Number with JavaScript:
+// You can easily decrement or decrease a variable by one with the -- operator.
+// i--;
+// is the equivalent of
+// i = i - 1;
+//************************* */
+let myar = 11;
+myar--;
+console.log(myar);
+
+
+
+
+// Create Decimal Numbers with JavaScript:
+// We can store decimal numbers in variables too.
+// Decimal numbers are sometimes referred to as floating point numbers or floats.
+let myDecimal=5.7;
+console.log(myDecimal);
+//Multiply Two Decimals with JavaScript
+const mul = 2.0 * 2.5;
+console.log(mul);
+//Divide One Decimal by Another with JavaScript
+const quotnt = 4.4 / 2.0;
+console.log(quotnt);
+//Finding a Remainder in JavaScript:
+const remaindr = 11%3;
+console.log(remaindr);
+
+//Compound Assignment With Augmented Addition:
+// In programming, it is common to use assignments to modify the contents of a variable. 
+// Remember that everything to the right of the equals sign is evaluated first, so we can say:
+// myVar = myVar + 5;
+// to add 5 to myVar. Since this is such a common pattern, 
+// there are operators which do both a mathematical operation and assignment in one step.
+// One such operator is the += operator.
+// let myVar = 1;
+// myVar += 5;
+// console.log(myVar);
+// 6 would be displayed in the console.
+let a = 3;
+let b = 17;
+let c = 12;
+a += 12;
+b += 9;
+c +=  7;
+//Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
+//Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+a *= 5;
+b *= 3 ;
+c *= 10;
+//Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+a /=12;
+b /= 4;
+c /= 11;
+
+//Escaping Literal Quotes in Strings:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
