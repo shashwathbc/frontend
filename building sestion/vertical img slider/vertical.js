1449d16ea7de43d78f01e7b5663af719fdcbaf28
Shashwath <shashwathbc@gmail.com>
2022-01-24 20:48:26 +0530

    commit

--- building sestion/vertical img slider/vertical.js
+++ building sestion/vertical img slider/vertical.js
@@ -1,15 +1,38 @@
-//sorting band:
+const sliderContainer = document.querySelector(".slider-container");
 
-//regex : remove those names  a , the ... 
-//replace 
-//sort
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
 
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
 
-
-
-
-
+const slideLength = slideRight.querySelectorAll("div").length;
 
 
-//drum kit:
-// data-key 
+let activeSlideIndex =0;
+
+slideLeft.getElementsByClassName.top = `-${(slideLength-1)*100}vh`
+
+upButton.addEventListener("click" , () => changeSlide("up"));
+downButton.addEventListener("click" , () => changeSlide("down"));
+
+
+
+const changeSlide =(direction) =>{
+    const sliderHeight = sliderContainer.clientHeight;
+
+    if(direction === "up"){
+        activeSlideIndex++;
+        if(activeSlideIndex > slideLength-1){
+            activeSlideIndex=0
+        }
+     }else if(direction === "down"){
+         activeSlideIndex--;
+         if(activeSlideIndex<0){
+             activeSlideIndex = slideLength-1;
+         }
+     }
+     slideRight.getElementsByClassName.transform = `translateY(-${activeSlideIndex*sliderHeight}px)`
+     slideLeft.getElementsByClassName.transform = `translateY(-${activeSlideIndex*sliderHeight}px)`
+    
+}
